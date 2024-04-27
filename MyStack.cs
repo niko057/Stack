4448637ffbfe4f38f71629f683c615a7f7d0173a
niko057 <nicatnamazov57.nn@gmail.com>
2024-04-28 00:35:32 +0400

    Add project files.

---
+++ MyStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class MyStack<T>
+    {
+        private T[] _items;
+        private int _top;
+
+        public MyStack(int size)
+        {
+            _items = new T[size];
+            _top = -1;
+        }
+
+        public void Push(T item)
+        {
+            if (_top == _items.Length - 1)
+            {
+                Array.Resize(ref _items, _items.Length * 2);
+            }
+
+            _top++;
+            _items[_top] = item;
+        }
+
+        public T Pop()
+        {
+            if (_top == -1)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+
+            T poppedItem = _items[_top];
+            _top--;
+            return poppedItem;
+        }
+
+        public T Peek()
+        {
+            if (_top == -1)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+
+            return _items[_top];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>(5);
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+            Console.WriteLine("Top element: " + stack.Peek()); 
+
+            Console.WriteLine("Popped element: " + stack.Pop()); 
+            Console.WriteLine("Top element after pop: " + stack.Peek()); 
+        }
+    }
+}
